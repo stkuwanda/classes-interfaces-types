77cad68a8d87289e812f8ab8ea667e7bceb663f6
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-17 22:17:49 +0200

    explored interfaces and type casting

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,45 @@
 // use it as a type
 // a type alias can be used to achieve something similar, like using it as
 // a type.
-type Person = {
-  name: string;
-  age: number;
-
+interface Greetable {
   greet(greeting: string): void;
 }
 
-const user: Person = {
-  name: 'Simba',
-  age: 34,
-  greet(greeting) {
-    console.log(`${greeting} ${this.name}.`);
-  },
+interface Shakable {
+  shakeHands(): void;
 }
 
-user.greet('Hi, my name is')
+// implementing multiple interfaces
+class Person implements Greetable, Shakable{
+  constructor(public name:string, public age: number) {}
+
+  greet(greeting: string): void {
+    console.log(`${greeting} ${this.name}.`);
+  }
+
+  shakeHands(): void {
+    console.log(`${this.name} is shaking hands with someone.`)
+  }
+  
+}
+
+const user = new Person('Simba', 34);
+user.greet('Hi, my name is');
+user.shakeHands();
+
+// explicit type casting 
+let shakableUser: Shakable = user as Shakable; // casting use `as` keyword
+shakableUser.shakeHands();
+
+let greetableUser: Greetable =  <Greetable> user; // casting using angle brackets
+greetableUser.greet('This greeting comes from a Software Engineer named');
+
+const user2 = new Person('Chichie', 28);
+
+// implicit type casting
+shakableUser = user2; // casting use `as` keyword
+shakableUser.shakeHands();
+
+greetableUser = user2; // casting using angle brackets
+greetableUser.greet('This greeting comes from a Software Engineer named');
+

fb15004f3a20a926fb2e5ca405cbfe8162119352
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-17 09:51:11 +0200

    used shorthand declaration of class properties as constructor parameters, updated tsconfig.json config to stop emitting comments

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 class Department {
-	name: string;
 	private employees: string[] = [];
 
-	constructor(n: string) {
-		this.name = n;
-	}
+	// shorthand declaration of class properties as constructor parameters
+	constructor(public id: string, public name: string) {}
 
-	describe(this: Department, code: string) { // type checking for any object that calls this function definition
-		console.log(`Department: ${this.name} : ${code}`);
+	describe(this: Department,) {
+		// type checking for any object that calls this function definition
+		console.log(`Department: ${this.name} : ${this.id}`);
 	}
 
 	addEmployee(employee: string) {
@@ -20,11 +19,11 @@ class Department {
 	}
 }
 
-const accounting = new Department('accounting');
+const accounting = new Department('ABX1', 'accounting');
 accounting.addEmployee('Simba');
 accounting.addEmployee('Chichie');
 // accounting.employees[3] = 'Christian'; // prevented access to employees property (private property)
-accounting.describe('ABX1');
+accounting.describe();
 accounting.printEmployeeInfor();
 // const accountingCopy = { name: 'accounting', describe: accounting.describe };
 // accounting.describe('ABX1'); // prints => Department: accounting

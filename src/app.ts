66a2ebefe4c3cdb7c860ee5560c37cec10e6a158
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-17 08:42:28 +0200

    updated describe method to accept a single argument

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ class Department {
 		this.name = n;
 	}
 
-	describe(this: Department) { // type checking for any object that calls this function definition
-		console.log(`Department: ${this.name}`);
+	describe(this: Department, code: string) { // type checking for any object that calls this function definition
+		console.log(`Department: ${this.name} : ${code}`);
 	}
 }
 
 const accounting = new Department('accounting');
 const accountingCopy = { name: 'accounting', describe: accounting.describe };
-accounting.describe(); // prints => Department: accounting
-accountingCopy.describe(); // prints => Department: undefined
+accounting.describe('ABX1'); // prints => Department: accounting
+accountingCopy.describe('ABX2'); // prints => Department: undefined

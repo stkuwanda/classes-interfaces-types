f56d0faa1d084771247191a4559f2c4a11779ea1
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-17 08:57:49 +0200

    demonstrated the use of a private property to limit access of a class property to within it's class

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
 	name: string;
+	private employees: string[] = [];
 
 	constructor(n: string) {
 		this.name = n;
@@ -8,9 +9,23 @@ class Department {
 	describe(this: Department, code: string) { // type checking for any object that calls this function definition
 		console.log(`Department: ${this.name} : ${code}`);
 	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInfor() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
 
 const accounting = new Department('accounting');
-const accountingCopy = { name: 'accounting', describe: accounting.describe };
-accounting.describe('ABX1'); // prints => Department: accounting
-accountingCopy.describe('ABX2'); // prints => Department: undefined
+accounting.addEmployee('Simba');
+accounting.addEmployee('Chichie');
+// accounting.employees[3] = 'Christian'; // prevented access to employees property (private property)
+accounting.describe('ABX1');
+accounting.printEmployeeInfor();
+// const accountingCopy = { name: 'accounting', describe: accounting.describe };
+// accounting.describe('ABX1'); // prints => Department: accounting
+// accountingCopy.describe('ABX2'); // prints => Department: undefined

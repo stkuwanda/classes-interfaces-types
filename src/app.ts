69c33600cac4476821342edd1db6e29884686c7b
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-18 12:21:34 +0200

    intersection types with type aliases, interfaces and custom types

--- src/app.ts
+++ src/app.ts
@@ -1,86 +1,52 @@
-// an interface defines the structure of an object but is non-instatiable.
-// use it as a type
-// a type alias can be used to achieve something similar, like using it as
-// a type.
-interface Greetable {
-	greet(greeting: string): void; // members are implicitly public
-  sendText?(text: string): void; // optional method
-}
+// type intersection
 
-// type Greetable = {
-// 	greet(greeting: string): void;
-// };
+type Combinable = string | number | symbol;
+type Selectable = boolean | number | symbol;
 
-interface Identifiable {
-	name: string;
-	age: number;
-	occupation?: string; // optional parameter
-}
+// type intersection with aliased union types.
+// the & operator will return the common type or
+// a union of types if there's more than one common type
+// into the alias.
+type Universal = Combinable & Selectable; // Universal aliases the returned union type (number | symbol)
 
-// interfaces can `extend` one or more other interfaces
-interface Shakable extends Identifiable {
-	shakeHands(): void;
-}
+const item: Universal = 6;
+const item2: Universal = Symbol('test');
+console.log(item, item2);
 
-// implementing multiple interfaces
-class Person implements Greetable, Shakable {
-	readonly name: string;
+// type intersection with aliased custom object types
+// the & operator will return a combined custom object type
+// into the declared alias
+type Admin = { privileges: string[] };
+type Employee = { name: string; startDate: Date };
+type ElevatedEmployee = Admin & Employee;
 
-	constructor(n: string, public age: number, public occupation?: string) {
-		this.name = n;
-	}
-
-	greet(greeting: string): void {
-		console.log(`${greeting} ${this.name}.`);
-	}
-
-	shakeHands(): void {
-		console.log(
-			`${this.name} is shaking hands with the ${this.occupation ?? 'someone'}.`
-		);
-	}
-}
-
-const user = new Person('Simba', 34, 'Doctor');
-user.greet('Hi, my name is');
-user.shakeHands();
-
-// explicit type casting
-let shakableUser: Shakable = user as Shakable; // casting use `as` keyword
-shakableUser.shakeHands();
-
-let greetableUser: Greetable = <Greetable>user; // casting using angle brackets
-greetableUser.greet('This greeting comes from a Software Engineer named');
-
-const user2 = new Person('Chichie', 28);
-
-// implicit type casting
-shakableUser = user2; // casting use `as` keyword
-shakableUser.shakeHands();
-
-greetableUser = user2; // casting using angle brackets
-greetableUser.greet('This greeting comes from a Software Engineer named');
-
-// function types with type aliases and interfaces
-
-// simple function type declaration
-const addNum: (a: number, b: number) => number = (a: number, b: number) =>
-	a + b;
-
-// type alias declaration
-type AddNumFunc = (a: number, b: number) => number;
-
-const addNumbers: AddNumFunc = function (a: number, b: number) {
-	return a + b;
+const specialUser: ElevatedEmployee = {
+	name: 'Simba',
+	startDate: new Date(),
+	privileges: ['super-user'],
 };
 
-// interface declaration
-interface AddNumberFunc {
-	(a: number, b: number): number;
+console.log(specialUser);
+
+
+// type intersection with interfaces.
+// the & operator will return a combined custom object type
+// into the declared alias, same as type intersection with
+// aliased custom object times.
+interface Authenticated {
+	privileges: string[];
 }
 
-const addNums: AddNumberFunc = (a: number, b: number) => a + b;
-
-function sumNumbersApp(a: number, b: number): number {
-	return a + b;
+interface Personnel {
+	name: string;
+	startDate: Date;
 }
+
+type Forewoman = Authenticated & Personnel;
+const manager: Forewoman = {
+	name: 'Chichie',
+	startDate: new Date(),
+	privileges: [''],
+};
+
+console.log(manager);
---
+++ src/interfaces.ts
@@ -0,0 +1,86 @@
+// an interface defines the structure of an object but is non-instatiable.
+// use it as a type
+// a type alias can be used to achieve something similar, like using it as
+// a type.
+interface Greetable {
+	greet(greeting: string): void; // members are implicitly public
+  sendText?(text: string): void; // optional method
+}
+
+// type Greetable = {
+// 	greet(greeting: string): void;
+// };
+
+interface Identifiable {
+	name: string;
+	age: number;
+	occupation?: string; // optional parameter
+}
+
+// interfaces can `extend` one or more other interfaces
+interface Shakable extends Identifiable {
+	shakeHands(): void;
+}
+
+// implementing multiple interfaces
+class Person implements Greetable, Shakable {
+	readonly name: string;
+
+	constructor(n: string, public age: number, public occupation?: string) {
+		this.name = n;
+	}
+
+	greet(greeting: string): void {
+		console.log(`${greeting} ${this.name}.`);
+	}
+
+	shakeHands(): void {
+		console.log(
+			`${this.name} is shaking hands with the ${this.occupation ?? 'someone'}.`
+		);
+	}
+}
+
+const user = new Person('Simba', 34, 'Doctor');
+user.greet('Hi, my name is');
+user.shakeHands();
+
+// explicit type casting
+let shakableUser: Shakable = user as Shakable; // casting use `as` keyword
+shakableUser.shakeHands();
+
+let greetableUser: Greetable = <Greetable>user; // casting using angle brackets
+greetableUser.greet('This greeting comes from a Software Engineer named');
+
+const user2 = new Person('Chichie', 28);
+
+// implicit type casting
+shakableUser = user2; // casting use `as` keyword
+shakableUser.shakeHands();
+
+greetableUser = user2; // casting using angle brackets
+greetableUser.greet('This greeting comes from a Software Engineer named');
+
+// function types with type aliases and interfaces
+
+// simple function type declaration
+const addNum: (a: number, b: number) => number = (a: number, b: number) =>
+	a + b;
+
+// type alias declaration
+type AddNumFunc = (a: number, b: number) => number;
+
+const addNumbers: AddNumFunc = function (a: number, b: number) {
+	return a + b;
+};
+
+// interface declaration
+interface AddNumberFunc {
+	(a: number, b: number): number;
+}
+
+const addNums: AddNumberFunc = (a: number, b: number) => a + b;
+
+function sumNumbersApp(a: number, b: number): number {
+	return a + b;
+}

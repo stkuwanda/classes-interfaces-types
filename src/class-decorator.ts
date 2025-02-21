489684a717ff046eb1e5861cd7c82e857800b0ce
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-02-21 08:08:24 +0200

    implemented another class decorator which can update the DOM

--- src/class-decorator.ts
+++ src/class-decorator.ts
@@ -59,3 +59,46 @@ class Foreman {
 const foreman = new Foreman() as any;
 console.log('foreman instance:', foreman);
 console.log('foreman employmentDateOnPrototype:', foreman.employmentDateOnPrototype);
+
+// another class decorator
+// factory
+function WithTemplate(template: string, hookId: string) {
+	// return the actual class decorator
+	// note; in a class decorator, the target parameter is a constructor function.
+	// this constructor function can be updated/replaced by a custom constructor.
+	// this update is done by returning the new function contructor from inside the
+	// decorator function.
+	// this whole exercise effectively updates or changes the class (constructor function) upon which this
+	// class decorator is applied.
+	return function <T extends new (...args: any[]) => { name: string }>(
+		target: T,
+		_: ClassDecoratorContext<T>
+	) {
+		// code to update class/constructor function
+		return class extends target {
+      age = 34;
+
+			constructor(...args: any[]) {
+				super();
+				console.log('Rendering template');
+				const hookEl = document.getElementById(hookId);
+
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector('h1')!.textContent = this.name;
+				}
+			}
+		};
+	};
+}
+
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+  name = 'Simba';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+// const pers = new Person();
